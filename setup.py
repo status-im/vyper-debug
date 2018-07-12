6fb0d103c77a06beed829bfce615626123298786
Jacques Wagener <jacques@dilectum.co.za>
2018-07-12 19:06:19 +0200

    Bump version: 0.1.0-alpha.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras_require['dev'] = (
 setup(
     name='vyper-debug',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.0',
+    version='0.1.1',
     description="""vyper-debug: Easy to use Vyper debugger | vdb""",
     long_description_markdown_filename='README.md',
     author='Jacques Wagener',
