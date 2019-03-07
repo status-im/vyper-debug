a9a9fd7d0b8bd895fb56c025ce6c921464074f2c
Jacques Wagener <jacques@dilectum.co.za>
2019-03-08 01:50:02 +0200

    Add global string print test.

--- tests/variables/test_print.py
+++ tests/variables/test_print.py
@@ -39,3 +39,25 @@ def test():
     c.functions.test().transact({"gas": 22000})
 
     assert get_last_out(stdout) == '-123'
+
+
+def test_print_string_from_storage(get_contract, get_last_out):
+    code = """
+car: public(string[64])
+
+@public
+def getCar() -> string[64]:
+    self.car = "My little car!"
+    vdb
+    return self.car
+    """
+
+    stdin = io.StringIO(
+        "self.car\n"
+    )
+    stdout = io.StringIO()
+
+    c = get_contract(code, stdin=stdin, stdout=stdout)
+    c.functions.getCar().call({"gas": 92000})
+
+    assert get_last_out(stdout) == "My little car!"
